785546a34dd0278b2daee81b17870d15a4f7a383
kaydurgu <mzhanbolot@gmail.com>
2023-12-13 17:10:24 +0300

    Connected to API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import CurrInput from './components/CurrInput';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
+
+  const [rates , setRates] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.apilayer.com/fixer/latest?base=KGS&apikey=wTGSZhXWhsoMaTb7sMBijxO3w8YaufE8')
+    .then(response => {
+      setRates(response.data.rates);
+    })
+  }, [])
   return (
     <div className="App">
         <h1> Zhanbolot</h1>
