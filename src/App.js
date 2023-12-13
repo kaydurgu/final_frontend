61792a5d9dd9667610c49b6afa62c5369d732698
kaydurgu <mzhanbolot@gmail.com>
2023-12-13 12:42:23 +0300

    Created component for entering a value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import React from 'react';
+import CurrInput from './components/CurrInput';
 function App() {
   return (
     <div className="App">
         <h1> Zhanbolot</h1>
+        <CurrInput/>
+        <CurrInput/>
     </div>
   );
 }
---
+++ src/components/CurrInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+function CurrInput(props){
+    return (
+        <div className="collection">
+            <input type="text" value={1}/>
+            <select></select>
+        </div>
+    );
+}
+export default CurrInput;
