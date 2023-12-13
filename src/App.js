c8f534907dba321dcf4f96bff989a07a16c1d09f
kaydurgu <mzhanbolot@gmail.com>
2023-12-13 17:38:17 +0300

    useEffect added for cheching if fetched data is not empty

--- src/App.js
+++ src/App.js
@@ -7,38 +7,65 @@ function App() {
 
   const [amount1, setAmount1] = useState(1);
   const [amount2, setAmount2] = useState(1);
-  const [currency1 ,setCurrency1] = useState('KGS');
-  const [currency2 ,setCurrency2] = useState('KGS');
+  const [currency1 ,setCurrency1] = useState('USD');
+  const [currency2 ,setCurrency2] = useState('USD');
   const [rates , setRates] = useState([]);
+  function format(number) {
+    return number.toFixed(4);
+  }
   useEffect(() => {
-    axios.get('https://api.apilayer.com/fixer/latest?base=KGS&apikey=wTGSZhXWhsoMaTb7sMBijxO3w8YaufE8')
+    axios.get('https://api.apilayer.com/fixer/latest?base=USD&apikey=wTGSZhXWhsoMaTb7sMBijxO3w8YaufE8')
     .then(response => {
       setRates(response.data.rates);
     })
   }, [])
-  
-  function handleAmount1Change(amount1){
-    setAmount2(amount1*rates[currency2] / rates[currency1]);
-    setAmount1(amount1);
 
+  useEffect(() => {
+    if (!!rates) {
+      function init() {
+        handleAmount1Change(1);
+      }
+      init();
+    }
+  }, [rates]);
+
+  function handleAmount1Change(amount1) {
+    setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+    setAmount1(amount1);
   }
+
+  function handleCurrency1Change(currency1) {
+    setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+    setCurrency1(currency1);
+  }
+
+  function handleAmount2Change(amount2) {
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+    setAmount2(amount2);
+  }
+
+  function handleCurrency2Change(currency2) {
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+    setCurrency2(currency2);
+  }
+
   return (
     <div className="App">
         <h1> Zhanbolot</h1>
         <CurrInput 
-        onAmountChange={setAmount1}
-        onCurrencyChange={setCurrency1}
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
         currencies={Object.keys(rates)} 
-        amount={1} 
-        currency={'KGS'} 
+        amount={amount1} 
+        currency={currency1} 
         />
 
         <CurrInput  
-        onAmountChange={setAmount2}
-        onCurrencyChange={setCurrency2}
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change}
         currencies={Object.keys(rates)} 
-        amount={1} 
-        currency={'KGS'} 
+        amount={amount2} 
+        currency={currency2} 
         /> 
     </div>
   );
