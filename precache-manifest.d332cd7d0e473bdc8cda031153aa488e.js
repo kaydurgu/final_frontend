2b238f111b99aab991380939353b2a504e649b9f
kaydurgu <mzhanbolot@gmail.com>
2023-12-13 19:47:03 +0300

    Updates

---
+++ precache-manifest.d332cd7d0e473bdc8cda031153aa488e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfea6915f20c06d49fd878d9bb639119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db60008ecaab4e650314",
+    "url": "/static/css/main.43219e0d.chunk.css"
+  },
+  {
+    "revision": "a5c6af65254febf4c272",
+    "url": "/static/js/2.43c17ff8.chunk.js"
+  },
+  {
+    "revision": "db60008ecaab4e650314",
+    "url": "/static/js/main.7c19712c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
