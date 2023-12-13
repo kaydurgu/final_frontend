b8ee8e82df467fcb28b0632dc731c189c5a37c0b
kaydurgu <mzhanbolot@gmail.com>
2023-12-13 17:25:27 +0300

    OnChange Functions added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 function App() {
 
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+  const [currency1 ,setCurrency1] = useState('KGS');
+  const [currency2 ,setCurrency2] = useState('KGS');
   const [rates , setRates] = useState([]);
   useEffect(() => {
     axios.get('https://api.apilayer.com/fixer/latest?base=KGS&apikey=wTGSZhXWhsoMaTb7sMBijxO3w8YaufE8')
@@ -12,11 +16,30 @@ function App() {
       setRates(response.data.rates);
     })
   }, [])
+  
+  function handleAmount1Change(amount1){
+    setAmount2(amount1*rates[currency2] / rates[currency1]);
+    setAmount1(amount1);
+
+  }
   return (
     <div className="App">
         <h1> Zhanbolot</h1>
-        <CurrInput currencies={['USD']} amount={1} currency={'USD'} />
-        <CurrInput currencies={['USD']} amount={1} currency={'USD'} /> 
+        <CurrInput 
+        onAmountChange={setAmount1}
+        onCurrencyChange={setCurrency1}
+        currencies={Object.keys(rates)} 
+        amount={1} 
+        currency={'KGS'} 
+        />
+
+        <CurrInput  
+        onAmountChange={setAmount2}
+        onCurrencyChange={setCurrency2}
+        currencies={Object.keys(rates)} 
+        amount={1} 
+        currency={'KGS'} 
+        /> 
     </div>
   );
 }
--- src/components/CurrInput.js
+++ src/components/CurrInput.js
@@ -1,14 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types"
 function CurrInput(props){
     return (
         <div className="collection">
-            <input type="text" value={props.amount}/>
-            <select value={props.currency}>
+            <input type="text" value={props.amount} onChange={
+              event => props.onAmountChange(event.target.value) 
+            }/>
+            <select value={props.currency} onChange={event => props.onCurrencyChange(event.target.value)}>
                 {props.currencies.map((currency => (
                     <option value={currency}>   {currency}   </option>
                 )))}
             </select>
         </div>
     );
+}
+CurrInput.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+    currencies: PropTypes.array,
+    onAmountChange : PropTypes.func,
+    onCurrencyChange : PropTypes.func,
 }
 export default CurrInput;
